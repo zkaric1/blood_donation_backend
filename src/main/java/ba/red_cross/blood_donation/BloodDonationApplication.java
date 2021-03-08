a52dac2ce59c385e277b3e087f91741a0900d35a
zkaric1 <zkaric1@etf.unsa.ba>
2021-03-08 20:00:10 +0100

    inital commit

---
+++ src/main/java/ba/red_cross/blood_donation/BloodDonationApplication.java
@@ -0,0 +1,13 @@
+package ba.red_cross.blood_donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodDonationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/red_cross/blood_donation/BloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.red_cross.blood_donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
